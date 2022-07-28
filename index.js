f4731cb3135d7d8c816b86f8c8c347c6592c4879
remmyssonkristhyan <rkristhyan@gmail.com>
2022-07-28 02:12:58 -0300

    criação da rota raiz

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 })
+
+app.get('/', (request, response) => {
+  return response.send('<h1>Trabalhando com servidor express</h1>');
+})
